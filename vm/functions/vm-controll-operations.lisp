85f5141ee76746a93190050c5829c60db6fddd81
Mohamed aziz Belhaj Hssine <azizhassine673@gmail.com>
2025-01-22 21:52:01 +0100

    Supprimé vm-controll-operations.lisp : Son contenu a été fusionné dans un fichier centralisé.

--- vm/functions/vm-controll-operations.lisp
+++
@@ -1,6 +0,0 @@
-(defun execute-nop (vm command)
-  ;; Intentionally does nothing, representing a no-operation instruction.
-  nil)
-
-(defun execute-halt (vm command)
-  (update-running vm 0))  ; Stops the execution of the VM
