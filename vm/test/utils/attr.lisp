e041aacebb928a2c0af081329a24f39e3059a34a
Mohamed aziz Belhaj Hssine <azizhassine673@gmail.com>
2025-01-22 23:18:43 +0100

    Supprimé test/utils/attr.lisp : Fichier obsolète ou intégré dans d'autres tests.

--- vm/test/utils/attr.lisp
+++
@@ -1,6 +0,0 @@
-(require "vm/utils/attr.lisp")
-
-;; Vérifie que attr-set/get fonctionne
-(let (vm '())
-  (attr-set vm :R0 10)
-  (format t "Test ATTR-SET, ATTR-GET: ~A~%" (= (attr-get vm :R0) 10)))
